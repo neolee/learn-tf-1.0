f4c62b07ad0812c8047419c53bd6c10da4c584cf
Neo Lee <neo.lee@gmail.com>
2017-04-10 23:04:50 +0800

    Add device log test

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
-sess = tf.Session()
 hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
 print(sess.run(hello))
 a = tf.constant(10)
 b = tf.constant(32)
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 print(sess.run(a + b))
